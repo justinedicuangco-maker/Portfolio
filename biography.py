6491fa91f2505d62c54dacdad8ae06864354709f
justinedicuangco-maker <justinedicuangco@gmail.com>
2026-01-11 13:06:54 +0000

    created the codes

---
+++ biography.py
@@ -0,0 +1,9 @@
+bio = {
+    "name": "Justine",
+    "hometown": "Manila",
+    "age": 18
+}
+
+print(bio["name"])
+print(bio["hometown"])
+print(bio["age"])
---
+++ simplesums.py
@@ -0,0 +1,9 @@
+# Declare and initialize variables
+num1 = 8
+num2 = 10
+
+# Calculate the sum
+total = num1 + num2
+
+# Print the result
+print(total)
